3a9045062f305eee28cb49c2be6230f8c720b72e
goingpaper <atonkin98@gmail.com>
2013-10-06 14:27:29 +0800

    made some changes to the models

--- project/barreviews/models.py
+++ project/barreviews/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Bar(models.Model):
-	bID = models.IntegerField() # possibly obselete
+
 	name = models.CharField(max_length=100) # bar name is not null and is not empty
 	address = models.CharField(max_length=100,blank = True,null = True) # address can be empty or null
 	phone = models.CharField(max_length=100) # unsure if this is right?????????!!!!!
@@ -12,6 +12,7 @@ class Bar(models.Model):
 	description = models.CharField(max_length=300,blank = True,null = True)
 	
 class User(models.Model):
+
 	username = models.CharField(max_length=100) # username compulsory
 	password = models.CharField(max_length=100) # password compulsory
 	fname = models.CharField(max_length=100) # fname compulsory
@@ -21,7 +22,44 @@ class User(models.Model):
 	user_type = models.CharField(max_length=100) #compulsory
 	
 class Drink(models.Model):
+
 	dID = models.IntegerField() 
 	brewery = models.CharField(max_length=100) 
-	dtype = models.CharField(max_length=100) 
+	type = models.CharField(max_length=100) 
 	name = models.CharField(max_length=100) 
+
+class Brewery(models.Model):
+
+	name = name = models.CharField(max_length=100) #not sure
+	
+class ReviewBar(models.Model):
+
+	username = models.ForeignKey('User') # ? constraints
+	barName = models.ForeignKey('Bar') # ? constraints
+	rating = models.IntegerField() #?
+	date = models.DateField()
+	comment = models.CharField(max_length=300)
+	#unable to make a multifield primary key in django
+
+class LikesBeer(models.Model):
+	
+	username = models.CharField(max_length=100) 
+	drinkName = models.ForeignKey('Drink')
+	
+class Serves(models.Model):
+	
+	barName = models.ForeignKey('Bar')
+	drinkName = models.ForeignKey('Drink')
+	onTap = models.BooleanField()
+	price = models.DecimalField(5,2) #max 5 digit number and 2 decimal places
+	
+class Comment(models.Model):
+
+	user1 = models.ForeignKey('User')
+	user2 = models.ForeignKey('User')
+	date = models.DateField()
+	comment = models.CharField(max_length=300)
+	
+
+	
+	
