93c643300014e004c19605320059632c34751c66
Brendan Pauley <21160544@ecm-csse2102-x.uniwa.uwa.edu.au>
2013-10-07 11:59:27 +0800

    Placeholder pages completed

--- project/barreviews/urls.py
+++ project/barreviews/urls.py
@@ -4,6 +4,10 @@ from django.views.generic import RedirectView
 from barreviews import views
 
 urlpatterns = patterns('',
-                       url(r'^$', RedirectView.as_view(url=reverse_lazy('barreviews:bars')), name='index'),
-                       url(r'^bars/$', views.BarsView.as_view(), name='bars')
+                       url(r'^$', RedirectView.as_view(url=reverse_lazy('barreviews:index')), name='indexr'),
+                       url(r'^index.html$', views.IndexView.as_view(), name='index'),
+                       url(r'^bars/$', views.BarsView.as_view(), name='bars'),
+                       url(r'^users/$', views.UsersView.as_view(), name='users'),
+                       url(r'^drinks/$', views.DrinksView.as_view(), name='drinks'),
+                       url(r'^reviews/$', views.ReviewsView.as_view(), name='reviews')
                        )
--- project/barreviews/views.py
+++ project/barreviews/views.py
@@ -1,9 +1,37 @@
 from django.views import generic
-from barreviews.models import *
+#from barreviews.models import *
+
+class IndexView(generic.ListView):
+    template_name = 'barreviews/index.html'
+    context_object_name = 'index'
+    
+    def get_queryset(self):
+        return (x for x in range(0,1))
 
 class BarsView(generic.ListView):
-    template_name = 'barreviews/index.html'
+    template_name = 'barreviews/bars.html'
     context_object_name = 'bars'
     
     def get_queryset(self):
         return (x for x in range(0,1)) #Bar.objects.all()
+
+class UsersView(generic.ListView):
+    template_name = 'barreviews/users.html'
+    context_object_name = 'users'
+    
+    def get_queryset(self):
+        return (x for x in range(0,1)) #User.objects.all()
+
+class DrinksView(generic.ListView):
+    template_name = 'barreviews/drinks.html'
+    context_object_name = 'drinks'
+    
+    def get_queryset(self):
+        return (x for x in range(0,1)) #Drink.objects.all()
+
+class ReviewsView(generic.ListView):
+    template_name = 'barreviews/reviews.html'
+    context_object_name = 'reviews'
+    
+    def get_queryset(self):
+        return (x for x in range(0,1)) #Review.objects.all()
