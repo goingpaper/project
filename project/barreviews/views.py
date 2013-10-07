54d0e8f909ef14b865b8039ad5c1f49c1c9b2602
Colkadome <josephdunne1993@hotmail.com>
2013-10-07 12:56:55 +0800

    Added more specific views

--- project/barreviews/views.py
+++ project/barreviews/views.py
@@ -35,3 +35,95 @@ class ReviewsView(generic.ListView):
     
     def get_queryset(self):
         return (x for x in range(0,1)) #Review.objects.all()
+        
+#More specific views below
+        
+class BarView(generic.DetailView):
+    model = Bar
+    template_name = 'barreviwes/bar.html'
+
+def bar_add(request):
+    if request.method == "POST":
+        form = BarForm(request.POST)
+        if form.is_valid():
+            bar = form.save()
+            return redirect('barreviews:bar', pk=bar.id)
+    else:
+        form = BarForm()
+    return render(request, 'barreviews/bar_add.html', {'form': form})
+
+def bar_edit(request, pk):
+    instance = Bar.objects.get(pk=pk)
+    if request.method == "POST":
+        form = BarForm(request.POST, instance = instance)
+        if form.is_valid():
+            car = form.save()
+            return redirect('barreviews:bar', pk=bar.id)
+    else:
+        form = BarForm(instance = instance)
+    return render(request, 'barreviews/bar_edit.html', {'form': form})
+
+def bar_delete(request, pk):
+    instance = Bar.objects.get(pk=pk)
+    instance.delete()
+    return redirect('laptimes:bars')
+
+class DrinkView(generic.DetailView):
+    model = Drink
+    template_name = 'barreviwes/drink.html'
+
+def drink_add(request):
+    if request.method == "POST":
+        form = DrinkForm(request.POST)
+        if form.is_valid():
+            drink = form.save()
+            return redirect('barreviews:drink', pk=drink.id)
+    else:
+        form = DrinkForm()
+    return render(request, 'barreviews/drink_add.html', {'form': form})
+
+def drink_edit(request, pk):
+    instance = Drink.objects.get(pk=pk)
+    if request.method == "POST":
+        form = DrinkForm(request.POST, instance = instance)
+        if form.is_valid():
+            drink = form.save()
+            return redirect('barreviews:drink', pk=drink.id)
+    else:
+        form = DrinkForm(instance = instance)
+    return render(request, 'barreviews/drink_edit.html', {'form': form})
+
+def drink_delete(request, pk):
+    instance = Drink.objects.get(pk=pk)
+    instance.delete()
+    return redirect('laptimes:drinks')
+
+class UserView(generic.DetailView):
+    model = User
+    template_name = 'barreviwes/user.html'
+
+def user_add(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('barreviews:user', pk=user.id)
+    else:
+        form = UserForm()
+    return render(request, 'barreviews/user_add.html', {'form': form})
+
+def user_edit(request, pk):
+    instance = User.objects.get(pk=pk)
+    if request.method == "POST":
+        form = UserForm(request.POST, instance = instance)
+        if form.is_valid():
+            user = form.save()
+            return redirect('barreviews:user', pk=user.id)
+    else:
+        form = UserForm(instance = instance)
+    return render(request, 'barreviews/user_edit.html', {'form': form})
+
+def user_delete(request, pk):
+    instance = User.objects.get(pk=pk)
+    instance.delete()
+    return redirect('laptimes:users')
