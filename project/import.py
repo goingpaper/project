7842f95517b7cc5c5b11a86c094a950afbec3e8d
goingpaper <atonkin98@gmail.com>
2013-10-14 10:40:41 +0800

    Update import.py
    updates some stuff

--- project/import.py
+++ project/import.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from django.core.exceptions import ValidationError
 from django.db import utils
 from barreviews.models import Bar, User, Drink, Brewery, ReviewBar, LikesBeer, Serves, Comment
+from django.contrib.auth.models import User
 
 #errors
 Bar.objects.all().delete()
@@ -13,6 +14,7 @@ ReviewBar.objects.all().delete()
 LikesBeer.objects.all().delete()
 Serves.objects.all().delete()
 Comment.objects.all().delete()
+User.objects.all().delete()
 
 with open('bars.json') as f:
 	bars_doc = json.load(f)
@@ -33,7 +35,11 @@ with open('drinks.json') as f:
 	drinks_doc = json.load(f)
 
 for drink in drinks_doc:
-	
+	if !Drink.objects.get('brewery'):
+		brewery = Brewery(name=drink.get('brewery'))
+		brewery.fulll_clean()
+		brewery.save()
+		
 	drink = Drink(brewery=drink.get('brewery'),
 				  dType=drink.get('type'),
 				  name = drink.get('name'))
